cc1888b599342a25cb37feb547b1ad4b0caa9112
BIT-USER <BIT-USER@DESKTOP-VEEGU4M>
2018-01-08 19:03:13 +0900

    6st

--- src/com/java/project1/Problem5.java
+++ src/com/java/project1/Problem5.java
@@ -5,17 +5,26 @@ import java.util.Scanner;
 public class Problem5 {
 
 	public static void main(String[] args) {
-		int i=0;
+	
 		Scanner scan = new Scanner(System.in);
 		System.out.println("숫자를 입력하세요");
 	
-		while(i<5) {
-			System.out.println("숫자:");
-			 i = scan.nextInt();
+		int array[]= new int[5];
+		 for(int i=1; i<5; i++) {
+			array [i] = scan.nextInt();
+		 }
 			 
+			int  max= array[0];//최대값 배열 0번째로 리턴 
 			
-			
-		}
+			for(int j=1; j<array.length; j++) {
+				if(array.length>max) {
+					max = array[j];
+				}
+				
+					
+			}
+				System.out.println("최대값은:"+max);
+		
 
 	}
 
