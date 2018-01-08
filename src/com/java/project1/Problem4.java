d7b75d93dbf99349e6a62fc7e1f7eb9e62c893e8
BIT-USER <BIT-USER@DESKTOP-VEEGU4M>
2018-01-08 19:21:11 +0900

    7st

--- src/com/java/project1/Problem4.java
+++ src/com/java/project1/Problem4.java
@@ -1,9 +1,25 @@
 package com.java.project1;
 
+import java.util.Scanner;
+
 public class Problem4 {
 
 	public static void main(String[] args) {
+		int num=0;
+		Scanner scan = new Scanner(System.in);
+		for(int i=1; i<=10; i++) {
+			
+			
+			for(int j=1; j<=10; j++) {
+				
+				System.out.print(num+j  +" ");
+			}
+		System.out.println();
+		//num이 늘어난다 1씩 
+		num++;
 		
+		}
+
 
 	}
 
